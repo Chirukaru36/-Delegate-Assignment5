707dd4e50224ca3a3ad0da2799c5af71dc22d0fa
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:16:02 +0530

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeaveRequestProgramLibrary;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            
+            employee.LeaveRequested += (requestedLeaves) =>
+            {
+                Console.WriteLine($"Leave request approved. Deducted {requestedLeaves} leaves. Leaves balance: {employee.LeavesBalance}");
+            };
+
+            Console.WriteLine("Employee leaves balance: " + employee.LeavesBalance);
+
+            
+            employee.RequestLeave(3);
+            employee.RequestLeave(5);
+            employee.RequestLeave(4);
+
+            Console.WriteLine("Final leaves balance: " + employee.LeavesBalance);
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
---
+++ LeaveRequestProgramLibrary/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveRequestProgramLibrary
+{
+    public class Employee
+    {
+        private int leaves;
+
+       
+        public delegate void LeaveRequestHandler(int requestedLeaves);
+
+     
+        public event LeaveRequestHandler LeaveRequested;
+
+        public Employee()
+        {
+            leaves = 10; 
+        }
+
+        public int LeavesBalance
+        {
+            get { return leaves; }
+        }
+
+   
+        public void RequestLeave(int requestedLeaves)
+        {
+            if (requestedLeaves <= leaves)
+            {
+                leaves -= requestedLeaves;
+
+               
+                LeaveRequested?.Invoke(requestedLeaves);
+            }
+            else
+            {
+                Console.WriteLine("Not enough leaves available for the request.");
+            }
+        }
+    }
+}
+
